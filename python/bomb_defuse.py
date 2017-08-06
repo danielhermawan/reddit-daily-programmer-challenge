5751a0eab73878798b46b8d3b9b7fe3eb51ae15d
Daniel Hermawan <danielhermawan96@gmail.com>
2017-08-07 01:25:05 +0700

    Bomb defused

--- python/bomb_defuse.py
+++ python/bomb_defuse.py
@@ -3,23 +3,30 @@ Easy Challenge
 Reddit Case Link: https://www.reddit.com/r/dailyprogrammer/comments/5e4mde/20161121_challenge_293_easy_defusing_the_bomb/
 """
 
-rules = {
-    'w': ['r', 'o', 'g', 'p'],
-    'r': ['g'],
-    'b': ['r', 'b', 'o'],
-    'o': ['r', 'b'],
-    'g': ['w', 'o'],
-    'p': ['r', 'b']
-}
+
+from typing import List
+
+
+def check_bomb(colors: List[str]) -> bool:
+    rules = {
+        'w': ['r', 'o', 'g', 'p'],
+        'r': ['g'],
+        'b': ['r', 'b', 'o'],
+        'o': ['r', 'b'],
+        'g': ['w', 'o'],
+        'p': ['r', 'b']
+    }
+    next = rules.keys()
+    valid = True
+    for i in range(len(colors)):
+        if colors[i] not in next:
+            valid = False
+            break
+        next = rules[colors[i]]
+    return valid
+
 colors = input('Please input order of color(w, r, b, o, g, p): ').split()
-next = rules.keys()
-valid = True
-for i in range(len(colors)):
-    if colors[i] not in next:
-        valid = False
-        break
-    next = rules[colors[i]]
-if valid:
+if check_bomb(colors):
     print("Bomb defused")
 else:
     print("Boom")
