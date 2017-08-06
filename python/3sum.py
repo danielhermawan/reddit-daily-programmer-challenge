df5884dadfb046bb1e5fc6896f265e4129a59895
Daniel Hermawan <danielhermawan96@gmail.com>
2017-08-07 00:23:33 +0700

    Update 3sum case description

--- python/3sum.py
+++ python/3sum.py
@@ -1,6 +1,24 @@
 """
 Easy Challenge
 Reddit Case Link: https://www.reddit.com/r/dailyprogrammer/comments/6melen/20170710_challenge_323_easy_3sum/
+
+Description
+    In computational complexity theory, the 3SUM problem asks if a given set of n real numbers contains three elements
+that sum to zero. A naive solution works in O(N2) time, and research efforts have been exploring the lower complexity
+bound for some time now.
+
+Input Example
+    You will be given a list of integers, one set per line. Example:
+    9 -6 -5 9 8 3 -4 8 1 7 -4 9 -9 1 9 -9 9 4 -6 -8
+
+Output Example
+    Your program should emit triplets of numbers that sum to 0. Example:
+    -9 1 8
+    -8 1 7
+    -5 -4 9
+    -5 1 4
+    -4 1 3
+    -4 -4 8
 """
 
 from itertools import combinations
